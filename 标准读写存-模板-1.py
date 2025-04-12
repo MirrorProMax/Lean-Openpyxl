0c5956c027925e1f2acfee265186297a377406e3
MirrorPro <644328850@qq.com>
2025-04-12 23:23:54 +0800

    重构Excel文件处理逻辑，优化变量命名并添加多行数据写入功能

--- 标准读写存-模板-1.py
+++ 标准读写存-模板-1.py
@@ -1,28 +1,32 @@
 from openpyxl import load_workbook
+from openpyxl.workbook.workbook import _WorksheetOrChartsheetLike
 from openpyxl.worksheet.worksheet import Worksheet
+from openpyxl.cell.cell import Cell
 
 filePath = "测试文件.xlsx"
 
-wb = load_workbook(filePath)  # 打开文件
+currentWorkbook = load_workbook(filePath)
 try:
-    ws = wb.active
-    if ws is None:
+    currentWorksheet = currentWorkbook.active
+    if currentWorksheet is None:
         raise ValueError("工作表不存在")
 
     # 读取单元格的值
-    theCell = ws["A1"]
-    print(theCell.value)
+    cellA: Cell = currentWorksheet["A1"]
+    print(cellA.value)
 
     # 写入单元格的值
-    ws["A1"] = "测试"
+    currentWorksheet["A1"] = "测试"
 
-    theList=[["标题1", "标题2", "标题3"],
-             ["内容1", "内容2", "内容3"],
-             ["内容4", "内容5", "内容6"]]
+    theList = [
+        ["标题1", "标题2", "标题3"],
+        ["内容1", "内容2", "内容3"],
+        ["内容4", "内容5", "内容6"],
+    ]
     # 写入多行数据
     for row in theList:
-        ws.append(row)
+        currentWorksheet.append(row)
     # 保存文件
-    wb.save(filePath)
+    currentWorkbook.save(filePath)
 finally:
-    wb.close()  # 确保文件被关闭
+    currentWorkbook.close()
